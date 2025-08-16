3c2b72b110f066f485ccf5d97bed9ec5bb1a5267
Ree Jay <real.anigan@gmail.com>
2025-08-16 16:39:19 +0800

    progress

--- legisvault/repository/urls.py
+++ legisvault/repository/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.indexview, name="index"),
+    path("aboutus", views.aboutUsView, name="aboutus"),
+    path("publications", views.publicationView, name="publications"),
+    path("publications-section", views.publicationSectionView, name="publications-section"),
 ]
--- legisvault/repository/views.py
+++ legisvault/repository/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def indexview(request):
-  return render(request,"../templates/home.html",{})
+  return render(request,"home.html",{"request":request})
+
+
+def aboutUsView(request):
+  return render(request,"aboutus.html",{})
+
+
+def publicationView(request):
+  return render(request,"publication.html",{"request":request})
+
+
+def publicationSectionView(request):
+  return render(request,"publication-section.html",{"request":request})
